af56e0b386616906ad45e373a8ea5177ea6801a9
brunosampaio222 <brunosampaio2223@gmail.com>
2023-12-03 10:46:50 -0300

    primeira execução do servidor

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const express=require("express")
 
 const app =express()
 
+app.get('/', (require,response)=> {
+    response.send("Olá Mundo")
+})
 
+app.listen(3000,()=> {
+    console.log("servidor Rodando Na porta 3000")
+})
